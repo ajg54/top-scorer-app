b45af0e3803d46444c4714807850d7d00f24b727
ajg54 <alexisgillett@hotmail.com>
2025-01-09 21:29:55 +0100

    Added loading of player form data from cricinfo.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
 
 # parameters
+agent = {"User-Agent": "Mozilla/5.0"}
 cricinfo_url = "https://www.espncricinfo.com"
 profile_path = cricinfo_url + "/cricketers/"
+stats_url_prefix = "https://stats.espncricinfo.com/ci/engine/player/"
+recent_matches_suffix = ".html?class=1;filter=advanced;orderby=start;orderbyad=reverse;template=results;type=batting;view=match"
 total_tests = 10
 sample_size = 1000000
-# load data
+# load local data
 choices = pd.read_csv("./data/choices.csv")
 choices.set_index('person', drop=True, inplace=True)
+choices['player_id'] = choices['cricinfo_path'].str.split("-").str[-1]
 choices['cricinfo_path'] = profile_path + choices['cricinfo_path']
 starting_stats = pd.read_csv("./data/starting_player_stats.csv")
 starting_stats.set_index('player', drop=True, inplace=True)
+# load remote data
+player_history = {}
+for player_name in choices['player']:
+    player_id = choices[choices['player'] == player_name]['player_id'].iloc[0]
+    url = stats_url_prefix + str(player_id) + recent_matches_suffix
+    player_history[player_name] = past_matches = pd.read_html(requests.get(url, headers=agent).text)[3]
 # initial calculations
 starting_stats['runs_per_match'] = starting_stats['runs'] / starting_stats['matches']
 starting_stats['initial_expected_runs'] = starting_stats['runs_per_match'] * total_tests
@@ -24,7 +35,15 @@ random_samples['winner'] = random_samples.idxmax(axis=1)
 winners = random_samples.groupby('winner').size()/sample_size
 winners.name = "Win Prob."
 starting_stats = starting_stats.join(other=winners)
-
+starting_stats['form'] = starting_stats.apply(lambda row: player_history[row.name]['Runs'].iloc[:10].sum(), axis=1)
+random_samples = pd.DataFrame()
+for player in starting_stats.index:
+    inverse_mean = 1 / (1+starting_stats.loc[player, 'form']/10)
+    random_samples[player] = np.random.negative_binomial(n=total_tests, p=inverse_mean, size=sample_size)
+random_samples['winner'] = random_samples.idxmax(axis=1)
+winners = random_samples.groupby('winner').size()/sample_size
+winners.name = "Form Prob."
+starting_stats = starting_stats.join(other=winners)
 # layout
 st.set_page_config(page_title="Top Scorer")
 st.title("England Test Top Scorer 2025")
@@ -35,4 +54,14 @@ st.data_editor(
 )
 st.dataframe(starting_stats.style.format({'runs_per_match': "{:.2f}",
                                           'initial_expected_runs': "{:.0f}",
-                                          'Win Prob.': "{:.2%}"}))
+                                          'Win Prob.': "{:.2%}",
+                                          'Form Prob.': "{:.2%}"}))
+st.write("Recent form:")
+player_tabs = st.tabs(list(starting_stats.index))
+for player_tab, player_name in zip(player_tabs, list(starting_stats.index)):
+    with player_tab:
+        player_id = choices[choices['player']==player_name]['player_id'].iloc[0]
+        url = stats_url_prefix + str(player_id) + recent_matches_suffix
+        st.write(url)
+        past_matches = player_history[player_name]
+        st.dataframe(past_matches[['Runs', 'Opposition', 'Ground', 'Start Date']], hide_index=True)
