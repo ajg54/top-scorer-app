07b25ce88859b595a15ff4d982a1b78868db4846
ajg54 <alexisgillett@hotmail.com>
2025-01-09 17:48:32 +0100

    Included basic player stat calculations.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 # parameters
 cricinfo_url = "https://www.espncricinfo.com"
 profile_path = cricinfo_url + "/cricketers/"
+total_tests = 10
 # load data
 choices = pd.read_csv("./data/choices.csv")
 choices.set_index('person', drop=True, inplace=True)
@@ -11,6 +12,7 @@ choices['cricinfo_path'] = profile_path + choices['cricinfo_path']
 starting_stats = pd.read_csv("./data/starting_player_stats.csv")
 starting_stats.set_index('player', drop=True, inplace=True)
 starting_stats['runs_per_match'] = starting_stats['runs'] / starting_stats['matches']
+starting_stats['initial_expected_runs'] = starting_stats['runs_per_match'] * total_tests
 
 # layout
 st.title("England Test Top Scorer 2025")
@@ -19,4 +21,5 @@ st.data_editor(
     choices,
     column_config={'cricinfo_path': st.column_config.LinkColumn()}
 )
-st.write(starting_stats)
+st.dataframe(starting_stats.style.format({'runs_per_match': "{:.2f}",
+                                          'initial_expected_runs': "{:.0f}"}))
