38ca589815273ade4ab3a6600baee83b39c3ad81
ajg54 <alexisgillett@hotmail.com>
2025-01-09 15:43:13 +0100

    Added data specifying the player selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
+
+
+choices = pd.read_csv("./data/choices.csv")
+choices.set_index('person', drop=True, inplace=True)
 
 st.title("England Test Top Scorer 2025")
-st.write(
-    "An app to keep track of a Cricket Cwappers bet."
-)
+st.write("An app to keep track of a Cricket Cwappers bet.")
+st.write(choices)
