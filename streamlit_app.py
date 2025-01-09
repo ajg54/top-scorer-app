7649f55888ca7781edd027bed3b913e76430e151
ajg54 <alexisgillett@hotmail.com>
2025-01-09 15:58:49 +0100

    Included link to cricinfo page about the players.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-
+# parameters
+cricinfo_url = "https://www.espncricinfo.com"
+profile_path = cricinfo_url + "/cricketers/"
+# load data
 choices = pd.read_csv("./data/choices.csv")
 choices.set_index('person', drop=True, inplace=True)
+choices['cricinfo_path'] = profile_path + choices['cricinfo_path']
 
+# layout
 st.title("England Test Top Scorer 2025")
 st.write("An app to keep track of a Cricket Cwappers bet.")
-st.write(choices)
+st.data_editor(
+    choices,
+    column_config={'cricinfo_path': st.column_config.LinkColumn()}
+)
