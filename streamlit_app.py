24d755de03514f9c5f20afa22f0069f2604503fb
ajg54 <alexisgillett@hotmail.com>
2025-01-09 17:33:32 +0100

    Added starting player stats.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ profile_path = cricinfo_url + "/cricketers/"
 choices = pd.read_csv("./data/choices.csv")
 choices.set_index('person', drop=True, inplace=True)
 choices['cricinfo_path'] = profile_path + choices['cricinfo_path']
+starting_stats = pd.read_csv("./data/starting_player_stats.csv")
+starting_stats.set_index('player', drop=True, inplace=True)
+starting_stats['runs_per_match'] = starting_stats['runs'] / starting_stats['matches']
 
 # layout
 st.title("England Test Top Scorer 2025")
@@ -16,3 +19,4 @@ st.data_editor(
     choices,
     column_config={'cricinfo_path': st.column_config.LinkColumn()}
 )
+st.write(starting_stats)
