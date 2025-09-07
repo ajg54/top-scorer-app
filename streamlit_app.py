c38fc86f645e9a177062de5f3b1005c32db61ecb
ajg54 <alexisgillett@hotmail.com>
2025-09-07 13:00:52 +0200

    Make app use full screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ pages_dir = "subpages"
 pg = st.navigation(
     [st.Page(os.path.join(pages_dir, "main.py"), title="Homepage", icon=":material/house:"),
      st.Page(os.path.join(pages_dir, "history.py"), title="History", icon=":material/history:")])
-st.set_page_config(page_icon=":material/sports_cricket:", page_title="Top Scorer")
+st.set_page_config(page_icon=":material/sports_cricket:", page_title="Top Scorer", layout="wide")
 pg.run()
